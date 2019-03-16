09b5f04e5dd420c4a77566e52cc629647808d924
Oriel Absin <Oriel Vinci Absin>
2019-03-16 23:02:23 +0800

    focus on map on country clicked

--- components/CountryList/index.js
+++ components/CountryList/index.js
@@ -1,18 +1,32 @@
+import { useContext } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Link from 'next/link';
 import ReactCountryFlag from "react-country-flag";
 
+import AppContainer from '../../state/AppContainer';
+
 const CountryList = ({ countries }) => {
+  const { changeMapPosition } = useContext(AppContainer.Context);
+
   console.log(countries)
+
   return ( 
     <List component="nav">
       {
         countries.map((country) => {
+          const { north, east, south, west } = country;
+          const bbox = {
+            north,
+            east,
+            south,
+            west
+          }
+
           return (
             <Link key={country.countryName} href={`/continent/country?name=${country.continentName}&country=${country.countryName}`}>
-              <ListItem button>
+              <ListItem button onClick={() => changeMapPosition({ bbox })}>
                 <ReactCountryFlag code={country.countryCode} svg />
                 <ListItemText primary={country.countryName} />
               </ListItem>

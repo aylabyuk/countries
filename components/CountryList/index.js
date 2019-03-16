fe02ac617c09995881b75d36d2109a3613f4c6ea
Oriel Absin <Oriel Vinci Absin>
2019-03-16 22:52:17 +0800

    add emoji flag

--- components/CountryList/index.js
+++ components/CountryList/index.js
@@ -2,6 +2,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Link from 'next/link';
+import ReactCountryFlag from "react-country-flag";
 
 const CountryList = ({ countries }) => {
   console.log(countries)
@@ -12,6 +13,7 @@ const CountryList = ({ countries }) => {
           return (
             <Link key={country.countryName} href={`/continent/country?name=${country.continentName}&country=${country.countryName}`}>
               <ListItem button>
+                <ReactCountryFlag code={country.countryCode} svg />
                 <ListItemText primary={country.countryName} />
               </ListItem>
             </Link>
--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,7 @@ class MyApp extends App {
       <Container>
         <Head>
           <title>Countries</title>
+          <meta charset="UTF-8" />
           {/* add leaflet cdn */}
           <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css"
             integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="

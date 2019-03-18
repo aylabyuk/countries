b2c336f8b404d13033e9f4d2dfab114b14651c6f
Oriel Absin <Oriel Vinci Absin>
2019-03-18 22:27:11 +0800

    fix layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
+
 import 'typeface-roboto';
 
 import AppContainer from './appContainer';
 import Layout from './components/Layout';
 
+
+// A theme with custom primary and secondary color.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: red[300],
+      main: red[500],
+      dark: red[700],
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
 class App extends Component {
   render() {
     return (
-      <AppContainer.Provider>
-        <Layout>
-          <div>test</div>
-        </Layout>
-      </AppContainer.Provider>
+      <MuiThemeProvider theme={theme}>
+        <AppContainer.Provider>
+          <Layout>
+            <div>test</div>
+          </Layout>
+        </AppContainer.Provider>
+      </MuiThemeProvider>
     );
   }
 }

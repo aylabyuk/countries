0deb5bf0340db9c45c7fe2a88de64c209a8921f1
Oriel Absin <Oriel Vinci Absin>
2019-03-19 12:25:24 +0800

    add basename

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import green from '@material-ui/core/colors/green';
 import { BrowserRouter as Router, Route } from "react-router-dom"
-
+ 
 import 'typeface-roboto';
 
 import AppContainer from './appContainer';
@@ -38,7 +38,7 @@ class App extends Component {
     return (
       <MuiThemeProvider theme={theme}>
         <AppContainer.Provider>
-        <Router>
+        <Router basename='/countries'>
           <Layout>
             <Route path="/" exact component={Home} />
             <Route path="/:continent" exact component={Continent} />

208c17ec9344bf0c11e8e152cf6446a34fc87ce0
Oriel Absin <Oriel Vinci Absin>
2019-04-18 11:01:06 +0800

    modify margin and padding for flag container

--- src/pages/Country.js
+++ src/pages/Country.js
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import { graphql, compose } from 'react-apollo';
 import { withStyles } from '@material-ui/core/styles';
+import ReactCountryFlag from "react-country-flag";
+
 import AppContainer from '../appContainer';
 import Loading from '../components/Loading';
-
 import GET_COUNTRY from '../graphql/queries/GET_COUNTRY';
 
 const styles = theme => ({
@@ -12,6 +14,12 @@ const styles = theme => ({
     padding: theme.spacing.unit * 3,
     fontWeight: 300,
     fontSize: 45
+  },
+  flagContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '10px auto 10px auto',
+    padding: '0px 10px 0px 10px'
   }
 })
 
@@ -34,6 +42,16 @@ const Country = ({ classes, location, cntry: { loading, country } }) => {
 
   return (
     <>
+      <Paper className={classes.flagContainer}>
+        <ReactCountryFlag
+          styleProps={{
+            width: '100px',
+            height: '100px'
+          }}
+          code={country.countryCode}
+          svg
+        />
+      </Paper>
       <Typography className={classes.countryName} color="textPrimary">{country.countryName}</Typography>
     </>
   )

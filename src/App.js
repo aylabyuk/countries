7200524100b2d25f78e29f2e21f6c7f6802ee101
Oriel Absin <oriel.absin@gmail.com>
2019-07-09 14:08:36 +0800

    use heroku provided url for production

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/red';
-import green from '@material-ui/core/colors/green';
-import { Router, Route } from "react-router-dom"
-import { createHashHistory } from 'history';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
- 
-import 'typeface-roboto';
+import React, { Component } from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import red from "@material-ui/core/colors/red";
+import green from "@material-ui/core/colors/green";
+import { Router, Route } from "react-router-dom";
+import { createHashHistory } from "history";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-import AppContainer from './appContainer';
-import Layout from './components/layout';
-import Home from './pages/Home';
-import Continent from './pages/Continent';
-import Country from './pages/country';
+import "typeface-roboto";
+
+import AppContainer from "./appContainer";
+import Layout from "./components/layout";
+import Home from "./pages/Home";
+import Continent from "./pages/Continent";
+import Country from "./pages/country";
 
 // A theme with custom primary and secondary color.
 const theme = createMuiTheme({
@@ -22,25 +22,25 @@ const theme = createMuiTheme({
       light: green[300],
       main: green[500],
       dark: green[700],
-      contrastText: '#fff',
+      contrastText: "#fff"
     },
     secondary: {
       light: red[300],
       main: red[500],
       dark: red[700],
-      contrastText: '#000',
-    },
+      contrastText: "#000"
+    }
   },
   typography: {
-    useNextVariants: true,
-  },
+    useNextVariants: true
+  }
 });
 
 const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
-})
+  uri: "https://pacific-forest-82775.herokuapp.com/graphql"
+});
 
 class App extends Component {
   render() {
@@ -48,7 +48,7 @@ class App extends Component {
       <ApolloProvider client={client}>
         <MuiThemeProvider theme={theme}>
           <AppContainer.Provider>
-            <Router  history={hashHistory}>
+            <Router history={hashHistory}>
               <Layout>
                 <Route path="/" exact component={Home} />
                 <Route path="/:continent" exact component={Continent} />
